6ce0720c04941df4dc4b42871be4cf574f177a10
Tomo Kaneko <richNEET@national.shitposting.agency>
2025-04-20 01:34:05 -0700

    Try printing more stuff out in src/main.zig and a build script for making a bootable image

--- make_image.sh
+++ make_image.sh
@@ -1,8 +1,8 @@
 # not really to be used but more of a note for myself
 qemu-img create -f raw zboot.img 64M
-mkfs -F32 zboot.img
+mkfs.fat -F 32 zboot.img
 mkdir efi_mount/
 sudo mount -o loop zboot.img efi_mount/
 sudo mkdir -p efi_mount/EFI/BOOT
-sudo cp zboot.img efi_mount/EFI/BOOT/BOOTX64.EFI
+sudo cp zig-out/x86_64-uefi/zboot.efi efi_mount/EFI/BOOT/BOOTX64.EFI
 sudo umount efi_mount
