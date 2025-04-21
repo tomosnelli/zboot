cf8109d7304831e8f9c405788d468b902e1c4834
Tomo Kaneko <richNEET@national.shitposting.agency>
2025-04-21 16:16:14 -0700

    Add make scripts. Maybe there is a better way than bash scripts

--- make_aarch64.sh
+++ make_aarch64.sh
@@ -1,11 +1,11 @@
 # not really to be used but more of a note for myself
-MOUNT="x86_64-uefi_mount"
-TRIPLE="x86_64-uefi"
-IMG="zboot-x86_64-uefi"
+MOUNT="aarch64-uefi_mount"
+TRIPLE="aarch64-uefi"
+IMG="zboot-aarch64-uefi"
 qemu-img create -f raw $IMG.img 64M
 mkfs.fat -F 32 $IMG.img
 mkdir $MOUNT/
 sudo mount -o loop $IMG.img $MOUNT/
 sudo mkdir -p $MOUNT/EFI/BOOT
-sudo cp zig-out/$TRIPLE/zboot.efi $MOUNT/EFI/BOOT/BOOTX64.EFI
+sudo cp zig-out/$TRIPLE/zboot.efi $MOUNT/EFI/BOOT/BOOTAA64.EFI
 sudo umount $MOUNT
