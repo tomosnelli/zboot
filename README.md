b6572dc43aee7755b1ea9b038c514a964a356e28
Tomo Kaneko <richNEET@national.shitposting.agency>
2025-04-21 15:12:46 -0700

    Add command note to README and refactor the code to be more clean

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ You will need OVMF
 ```bash
 sudo pacman -S ovmf
 ```
+
+## Run on Qemu command
+```bash
+qemu-system-x86_64 -bios /usr/share/ovmf/x64/OVMF.4m.fd -drive file=zboot.img,format=raw -m 4G
+```
--- src/main.zig
+++ src/main.zig
@@ -1,51 +1,33 @@
 const uefi = @import("std").os.uefi;
 const unicode = @import("std").unicode;
 
+fn console_out(con: *uefi.protocol.SimpleTextOutput, msg: [*:0]const u16) !void {
+    _ = con.outputString(msg) catch |err| {
+        return err;
+    };
+}
+
 // pub export fn efi_main(handle: uefi.Handle, system_table: *uefi.tables.SystemTable) callconv(.C) uefi.Status {
 pub export fn main() callconv(.C) uefi.Status {
-    // _ = handle;
-
     const con_out = uefi.system_table.con_out.?;
     const boot_serv = uefi.system_table.boot_services.?;
     const message = unicode.utf8ToUtf16LeStringLiteral("Hello from Zig!\r\n");
 
-    con_out.reset(false) catch |err| {
-        // issue
-        switch (err) {
-            error.Unexpected => return uefi.Status.aborted,
-            error.DeviceError => return uefi.Status.device_error,
-            else => unreachable,
-        }
+    _ = con_out.reset(false) catch |err| {
+        return uefi.Status.fromError(@errorCast(err));
     };
 
-    // _ = con_out.outputString(&[_:0]u16{ 'H', 'e', 'l', 'l', 'o', ' ', 'f', 'r', 'o', 'm', ' ', 'z', 'i', 'g', '!', '\n', 0 }) catch |err| {
-    _ = con_out.outputString(@ptrCast(message)) catch |err| {
-        // issue
-        switch (err) {
-            error.Unexpected => return uefi.Status.aborted,
-            error.DeviceError => return uefi.Status.device_error,
-            else => unreachable,
-        }
+    _ = console_out(con_out, @ptrCast(message)) catch |err| {
+        return uefi.Status.fromError(@errorCast(err));
     };
 
-    _ = con_out.outputString(@ptrCast(uefi.system_table.firmware_vendor)) catch |err| {
-        // issue
-        switch (err) {
-            error.Unexpected => return uefi.Status.aborted,
-            error.DeviceError => return uefi.Status.device_error,
-            else => unreachable,
-        }
+    _ = console_out(con_out, uefi.system_table.firmware_vendor) catch |err| {
+        return uefi.Status.fromError(@errorCast(err));
     };
 
     const status = boot_serv.stall(5 * 1000 * 1000);
-    if (status != uefi.Status.success) {
-        _ = con_out.outputString(&[_:0]u16{ 'E', 'r', 'r', 'o', 'r', '!', '\n', 0 }) catch |err| {
-            switch (err) {
-                error.Unexpected => return uefi.Status.aborted,
-                error.DeviceError => return uefi.Status.device_error,
-                else => unreachable,
-            }
-        };
+    if (status != .success) {
+        return status;
     }
 
     return uefi.Status.success;
